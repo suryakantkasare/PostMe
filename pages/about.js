4e4420f9ca98d5a71ecc688ce3c4790c68dc8c2e
Surykant Kasare <kasare9096464112@gmail.com>
2023-05-28 14:05:46 +0530

    add a function for getStaticProps

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+export const getStaticProps = () => {
+  const allTopics = getAllTopics();
+  return {
+    props: {
+      topics: allTopics,
+    },
+  };
+};
 export default function about() {
   return (
-    <div>about</div>
+    <div></div>
   )
 }
