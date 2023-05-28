b27af0cf98fb71896b34a1efda8dbe10cafe2995
Surykant Kasare <kasare9096464112@gmail.com>
2023-05-28 15:18:50 +0530

    setup of Components

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,5 @@
 import React from 'react'
 
-export const getStaticProps = () => {
-  const allTopics = getAllTopics();
-  return {
-    props: {
-      topics: allTopics,
-    },
-  };
-};
 export default function about() {
   return (
     <div></div>
