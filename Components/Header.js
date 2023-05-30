9f4f151693e38c84983cceec1240ad1319fea51b
Surykant Kasare <kasare9096464112@gmail.com>
2023-05-30 13:33:33 +0530

    home page update

--- Components/Header.js
+++ Components/Header.js
@@ -16,11 +16,10 @@ function Header({ topic, topicName, topicCount }) {
           <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
 
             <h1 className="mb-8 text-4xl font-bold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight dark:text-gray-50">
-              <span>Explore</span>{" "}
               <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-blue-400 to-purple-500 lg:inline">
                 PostMe
               </span>{" "}
-              <span>🚀</span>
+            🚀
             </h1>
           </div>
         </div>
